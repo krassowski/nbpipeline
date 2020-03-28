1365d3e96bf6c72ccd55c12f8bca1f112dab08df
krassowski <krassowski.michal@gmail.com>
2020-03-28 18:29:17 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         name='nbpipeline',
         packages=find_packages(),
         scripts=['bin/nbpipeline'],
-        version='0.1.17',
+        version='0.1.18',
         license='MIT',
         package_data={'nbpipeline': ['visualization/templates/*', 'visualization/templates/static/*']},
         include_package_data=True,
