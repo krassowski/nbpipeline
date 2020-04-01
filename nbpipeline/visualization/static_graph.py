35161b7d304effe5589284fbe7b66f007222f4c0
krassowski <krassowski.michal@gmail.com>
2020-04-01 10:55:17 +0100

    Fix static plot generation, ensure status is ok

--- nbpipeline/visualization/static_graph.py
+++ nbpipeline/visualization/static_graph.py
@@ -12,20 +12,21 @@ def static_graph(rules_dag):
     with NamedTemporaryFile(suffix='.dot') as dot_file:
         graph.write(dot_file.name)
 
-    graph.clear()
+        graph.clear()
 
-    with NamedTemporaryFile(mode='r', suffix='.svg') as temp_file:
-        system(f'dot -Tsvg {dot_file.name} -o {temp_file.name}')
+        with NamedTemporaryFile(mode='r', suffix='.svg') as temp_file:
+            status = system(f'dot -Tsvg {dot_file.name} -o {temp_file.name}')
+            assert status == 0
 
-        # with open(temp_file.name) as f:
-        svg = temp_file.read()
-        insert_after = 'xmlns:xlink="http://www.w3.org/1999/xlink">'
-        processed = svg.replace(insert_after, insert_after + """
-            <style>
-            a:hover polygon {
-                fill: red;
-            }
-            </style>
-        """)
+            # with open(temp_file.name) as f:
+            svg = temp_file.read()
+            insert_after = 'xmlns:xlink="http://www.w3.org/1999/xlink">'
+            processed = svg.replace(insert_after, insert_after + """
+                <style>
+                a:hover polygon {
+                    fill: red;
+                }
+                </style>
+            """)
 
     return processed
